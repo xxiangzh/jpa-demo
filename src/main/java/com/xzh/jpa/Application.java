ece733c058ca612c2cd9e6fd325fab8720ba3dcb
xzh <anenan@qq.com>
2020-10-31 10:47:01 +0800

    first commit

---
+++ src/main/java/com/xzh/jpa/Application.java
@@ -0,0 +1,19 @@
+package com.xzh.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动
+ *
+ * @author: 向振华
+ * @date: 2020/10/31 10:56
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
